a57c19a67fd10e0eec938867b5894f98f2b1e19e
Jose Manuel Ortiz Santiago <joma.ostgo@outlook.com>
2026-01-20 22:48:42 -0600

    LISTO PARA AGREGAR LO DE LAS IMPRESORAS
    -SE  AGREGO LA COLUMNA DE IMPRESORAS
    -SE CREO EL CONTROLADOR IMPRESORACONTROLLER
    -SE CREO LA CARPETA EN VIEWS LLAMADA IMPRESORA
    -SE CREO LA VISTA IMPRESORAS EN LA CARPETA IMPRESORA

---
+++ Controllers/ImpresoraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MantenimientosTI.Controllers
+{
+    public class ImpresoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
